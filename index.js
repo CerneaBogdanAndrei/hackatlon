805d57949bf5be9fd9241c362958a5c1dad8e002
Ionita <stefan.ionuta2014@gmail.com>
2025-11-24 17:54:26 +0200

    punere in teste

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const locationsRaw = require("./src/data/locations.json");
+
+console.log("locations count:", locationsRaw.length);
+console.log("first raw location:", locationsRaw[0]);
+
+
+const locations = locationsRaw.map((l) => ({
+    name: l.name,
+    address: l.address,
+    latitude: l.coordinates.lat,
+    longitude: l.coordinates.long,
+    image_url: l.image_url,
+    short_description: l.short_description,
+    rating: l.rating,
+}));
+
+console.log("first normalized location:", locations[0]);
+
+
+module.exports = { locations };
